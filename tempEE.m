28ea1ad326890eccece211de53e750ba0a4ddd66
davidweicker <david.weicker@student.uclouvain.be>
2015-10-14 17:27:22 +0200

    padam padam

--- tempEE.m
+++ tempEE.m
@@ -31,12 +31,15 @@ end
 u(1,:) = boundCondLeft(0:ht:tend);
 
 %On fait un joli movie de la temperature :)
-% for i=1:length(t)
-%     titre = sprintf('Time t=%f',(i-1)*ht);
-%     plot(x,u(:,i));axis([0 1 0 1]);title(titre);
-%     F(i)=getframe;
-% end
-% movie(F);
+for i=1:length(t)
+    titre = sprintf('Time t=%f',(i-1)*ht);
+    bar = u(:,i)*[1 1];
+    contourf(x,1:2,bar',0:0.01:1);title(titre);colorbar;caxis([0 1]);
+    F(i)=getframe;
+end
+movie(F);
+
+u = flipud(u);
 end
 
 function U = boundCondLeft(t)
